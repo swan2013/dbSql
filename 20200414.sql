1b20c8413f65dccb9a7aca0a1631af41d8830171
swan2013 <dkstmddnjs2@gmail.com>
2020-04-16 13:45:04 +0900

    sql

---
+++ 20200414.sql
@@ -0,0 +1,11 @@
+/* SQL 주석 */
+--한줄 주석 --> JAVA 의 한줄 주석 //
+/* 여러줄 주석 */
+
+SELECT *    -- 모든 컴럼의 정보를 조회
+FROM prod;  -- prod 테이블에서
+
+SQL 실행하는 방법
+1. 실행하는 SQL을 선택하고 (shift) ctrl + enter;
+2. tlfgodgkfusms SQL문장 아무곳에서나 프롬프트를 위치시키고 ctrl + enter;
+    *단 위아래 다른 sql 문장이 있을 경우 ;에 의해 sql이 구분되지 못할경우 실행되지 못함
